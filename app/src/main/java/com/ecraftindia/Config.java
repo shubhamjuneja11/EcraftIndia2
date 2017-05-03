ea8bc83613da515b6600298cd6f0539be36dc9ee
shubham <shubhamjuneja11@gmail.com>
2017-05-04 02:23:57 +0530

    Signin done

---
+++ app/src/main/java/com/ecraftindia/Config.java
@@ -0,0 +1,11 @@
+package com.ecraftindia;
+
+/**
+ * Created by hp on 02-May-17.
+ */
+
+public class Config {
+    public static String URL_LOGIN = "http://10.0.2.2/login_api/login.php";
+    public static String URL_REGISTER = "http://10.0.2.2/login_api/register.php";
+
+}
